a6c0253883b1a681006aa7ec3e8dbd958e16f476
Bakhtiyor Khasanov <bkhasanov77@gmail.com>
2023-02-18 11:53:28 +0500

    initial commit: created code base

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
