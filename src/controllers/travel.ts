0c368c404858189da643b98ecfa3022b93be5384
Bakhtiyor Khasanov <bkhasanov77@gmail.com>
2023-02-20 14:00:13 +0500

    added CRUD functions

--- src/controllers/travel.ts
+++ src/controllers/travel.ts
@@ -1,13 +1,25 @@
 import { Request, Response } from 'express'
-import Tarvel, { validate } from '../models/travel.js'
+import Travel, { validate } from '../models/travel.js'
 
 // Method: GET - get all travels
 export const getAllTravels = async (req: Request, res: Response) => {
   try {
-    const travels = await Tarvel.find()
+    const travels = await Travel.find()
     res.status(200).json({ message: 'ok', travels })
   } catch (err) {
-    res.status(404).send(err)
+    res.send(err)
+  }
+}
+
+// Method: GET - get one travel by id
+export const getOneTravel = async (req: Request, res: Response) => {
+  try {
+    const travel = await Travel.findById(req.params.id)
+    if (!travel) return res.status(404).send('Not found!')
+
+    res.status(200).json({ message: 'ok', travel })
+  } catch (err) {
+    res.send(err)
   }
 }
 
@@ -15,13 +27,40 @@ export const getAllTravels = async (req: Request, res: Response) => {
 export const addNewTravel = async (req: Request, res: Response) => {
   try {
     const { value, error } = validate(req.body)
-
     if (error) return res.status(400).json(error.details)
 
-    const travel = await Tarvel.create(value)
-
+    const travel = await Travel.create(value)
     res.status(201).json({ message: 'ok', travel })
   } catch (err) {
-    res.status(404).send(err)
+    res.send(err)
+  }
+}
+
+// Method: PUT - update travel
+export const updateTravel = async (req: Request, res: Response) => {
+  try {
+    const { value, error } = validate(req.body)
+    if (error) return res.status(400).json(error.details)
+
+    const updatedTravel = await Travel.findByIdAndUpdate(req.params.id, value, {
+      returnDocument: 'after',
+    })
+    if (!updatedTravel) return res.status(404).send('Not found!')
+
+    res.status(200).json({ message: 'updated', updatedTravel })
+  } catch (err) {
+    res.send(err)
+  }
+}
+
+// Method: DELETE - delete travel
+export const deleteTravel = async (req: Request, res: Response) => {
+  try {
+    const deletedTravel = await Travel.findByIdAndRemove(req.params.id)
+    if (!deletedTravel) return res.status(404).send('Not found!')
+
+    res.status(200).json({ message: 'deleted' })
+  } catch (err) {
+    res.send(err)
   }
 }
--- src/routes/travel.ts
+++ src/routes/travel.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express'
-import { getAllTravels, addNewTravel } from '../controllers/travel.js'
+import { getAllTravels, addNewTravel, getOneTravel, updateTravel, deleteTravel } from '../controllers/travel.js'
 
 const router = Router()
 
 router.get('/', getAllTravels)
+router.get('/:id', getOneTravel)
 router.post('/add', addNewTravel)
+router.put('/:id', updateTravel)
+router.delete('/:id', deleteTravel)
 
 export default router
